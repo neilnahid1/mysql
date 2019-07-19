c7b7bc4dd9f8b70e67e63a674e086f7a06fef886
Neil Francis Nahid <neil.nahid@fullspeedtechnologies.com>
2019-07-19 14:13:47 +0800

    updated exercises6 to use case statements

--- intermediate/exercise6.sql
+++ intermediate/exercise6.sql
@@ -1,12 +1,9 @@
-select employeeNumber as ID, -- 1st col
-       concat_ws(' ',firstname,lastname) as Employee, -- 2nd col
-       if(count(customers.customerNumber)>=6, -- condition 
-          employees.salary*0.30, -- true 
-          if(count(customers.customerNumber)>0, -- false, condition
-            employees.salary*0.10, -- true
-            0 -- false
-            )
-         ) as 'Bonus' -- 3rd col
+select employeeNumber as ID, concat_ws(' ',firstname,lastname) as Employee,
+       case 
+       when count(customers.customerNumber)>= 6 then employees.Salary*0.30
+       when count(customers.customerNumber) > 0 and count(customers.customerNumber) < 6 then employees.salary*0.10
+       else 0.0
+       end as 'Bonus'
 from employees
 left join customers on employees.employeeNumber=customers.salesrepemployeeNumber
 where employees.jobTitle='Sales Rep'
