36f7232da3135d3fc4d111ba8c7610ec7068bf08
Neil Francis Nahid <neil.nahid@fullspeedtechnologies.com>
2019-07-22 12:07:47 +0800

    updated users table sql

--- webappdb/create_users.sql
+++ webappdb/create_users.sql
@@ -2,6 +2,8 @@ CREATE TABLE Users(
 UserID INT PRIMARY KEY AUTO_INCREMENT,
 FirstName varchar(45),
 LastName varchar(45),
+Username varchar(45),
+Password varchar(45),
 RoleID INT not null,
 FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)
 )
